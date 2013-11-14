c11082ef5f5fbcb8619d34bf15993b2ae6499983
Diego Galaz <dgalaz@ucsd.edu>
2013-11-13 22:32:27 -0800

    started instruction rom for program 1

--- instruction ROM/instruction_rom.v
+++ instruction ROM/instruction_rom.v
@@ -1,33 +1,25 @@
 module instruction_rom_prog1
 (
-    input clock,
+    //input clock,
     input [7:0] address,
     output [8:0] instruction,
 );
 
-    reg [7:0]  rom[255:0];
+    reg [8:0]  instruction_out;
 
     //TODO: NEED TO ADD PROGRAM CODE IN ROM
-    initial begin
-
-        rom[0] = 8'b00000000;
-        rom[1] = 8'b00000000;
-        rom[2] = 8'b00000000;
-        rom[3] = 8'b00000000;
-        rom[4] = 8'b00000000;
-        rom[5] = 8'b00000000;
-        rom[6] = 8'b00000000;
-        rom[7] = 8'b00000000;
-        rom[8] = 8'b00000000;
-        rom[9] = 8'b00000000;
-        rom[10] = 8'b00000000;
-
-
+    always @ (address) begin
+        case (address)
+            0:  instruction_out = 9'b1010_0_0001;    // set_low  $imm, 0001
+            1:  instruction_out = 9'b0001_10_001;    // load     $t1, $imm
+            2:  instruction_out = 9'b0110_10_100;    // set_from $t1, $s1
+            3:  instruction_out = 9'b0111_01_000;    // incr     $imm
+            4:  instruction_out = 9'b0001_10_001;    // load     $t1, $imm
+            5:  instruction_out = 9'b0110_10_101;    // set_from $t1, $s2
+            6:  instruction_out = 9'b0101_10_000;    // set_to   $t1, $zero
+        endcase
     end
 
-    always @ (posedge clock){
-        instruction = clock[address];
-    }
-
+    assign instruction = instruction_out;
 
 endmodule
